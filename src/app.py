df7a350ca356602c5c220de35b0d8f0dbb1a4668
chankoo <ckbaek1125@gmail.com>
2025-08-03 19:09:49 +0900

    test: write department_df

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from services.data import departments
 
 st.title("Hello, Streamlit!")
 
 st.write("This is a simple Streamlit app.")
+
+
+st.write(departments.department_df)
